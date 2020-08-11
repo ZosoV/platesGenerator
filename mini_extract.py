1e48e5a416416d4fe9c24660f2f96e5c786a1199
ZosoV <hoskarg9619@gmail.com>
2020-08-11 14:56:48 -0500

    Create the mini extract

--- mini_extract.py
+++ mini_extract.py
@@ -1,16 +1,19 @@
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
+#!/usr/bin/env python
+
 import shutil
+import os
+import sys
 
 source = "bgs/"
 destination = "mini_bgs"
-os.mkdir("mini_bgs")
 destination = destination + "/" 
 
-num_image = int(sys.argv[1])
+if __name__ == "__main__":
+    os.mkdir("mini_bgs")
+    num_image = int(sys.argv[1])
 
-for i in range(1,num_image + 1):
-    str_m = str(i)
-    str_m = str_m.zfill(12)
-    if(not str_m in set_m):
-        shutil.copy(source+str_m+".png", destination+str_m+".png")
+    for i in range(1,num_image + 1):
+        str_m = str(i)
+        str_m = str_m.zfill(8)
+        if(not str_m in set_m):
+            shutil.copy(source+str_m+".png", destination+str_m+".png")
