57f2ad3971d37572c0593820ce2e871ba08356b9
ZosoV <hoskarg9619@gmail.com>
2020-08-14 01:14:49 -0500

    Adjusting the variations in gen.py

--- gen.py
+++ gen.py
@@ -201,7 +201,7 @@ def generate_bg(num_bg_images):
     '''    
     found = False
     while not found:
-        fname = "mini_bgs/{:08d}.jpg".format(random.randint(1, num_bg_images))
+        fname = "mini_bgs/{:08d}.jpg".format(random.randint(1, num_bg_images - 1))
         bg = cv2.imread(fname, cv2.IMREAD_GRAYSCALE) / 255.
         if (bg.shape[1] >= OUTPUT_SHAPE[1] and
             bg.shape[0] >= OUTPUT_SHAPE[0]):
@@ -305,7 +305,7 @@ def parse_args():
                         help='Chose the font of the plates')
     parser.add_argument('--format', type=int, default=1,
                         help='Chose the correct format 1: current and 2: past format')
-    parser.add_argument('--star-idx', type=int, default=5602,
+    parser.add_argument('--star-idx', type=int, default=5203,
                         help='Chose the index to start the names of images')
     return parser.parse_args()
 
