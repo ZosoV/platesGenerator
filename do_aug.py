be009146653d928863a4468ce5aca7f8dec4cb11
ZosoV <hoskarg9619@gmail.com>
2020-10-14 10:23:36 -0500

    changes of augmentation

--- do_aug.py
+++ do_aug.py
@@ -14,9 +14,6 @@ mini_aug_dir = "mini_aug"
 if not os.path.exists(augmentation_dir):
     os.mkdir(augmentation_dir)
 
-# if not os.path.exists(mini_aug_dir):
-#     os.mkdir(mini_aug_dir)
-
 images= glob.glob(path)
 
 for idx, img_path in enumerate(images):
@@ -27,22 +24,3 @@ for idx, img_path in enumerate(images):
     rd_image = am.random_brightness(image,random_interval)
     print("Augmented image: {}  Type: {}".format(os.path.join(augmentation_dir,img_name),type_plate))
     cv2.imwrite(os.path.join(augmentation_dir,img_name),rd_image)
-
-
-# Proof with little sample of plates
-
-# tipo 1 -1.6
-# tipo 2 -1.3
-
-# images= glob.glob(path)
-
-# for idx, img_path in enumerate(images[0:6]):
-#     img_name = img_path.split('/')[-1]
-    
-#     image = cv2.imread(img_path)
-#     rd_image = am.change_light_by_hsv(image,0.3)
-#     print("Augmented image: {}".format(os.path.join(mini_aug_dir,img_name)))
-#     cv2.imwrite(os.path.join(mini_aug_dir,img_name),rd_image)
-
-# random_brig_imgs = am.random_brightness(images,(0.35,1.5)) 
-# hp.visualize(random_brig_imgs, column=3)
